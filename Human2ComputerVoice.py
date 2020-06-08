3c377d27d291664e0be10a0852838413e00c9a50
Sandeep Bari <64518228+SandeepBari@users.noreply.github.com>
2020-06-08 12:44:26 +0530

    Human To Computer Voice
    Life Hacks

---
+++ Human2ComputerVoice.py
@@ -0,0 +1,30 @@
+import speech_recognition as spr
+from googletrans import Translator
+from gtts import gTTS
+import os
+
+recog = spr.Recognizer()
+translator = Translator()
+from_lang = 'en'
+to_lang = 'hi'
+
+with spr.Microphone() as source:
+    print('You Can Speak Now')
+    audio = recog.listen(source)
+    get_sentence = recog.recognize_google(audio)
+        
+    try:
+        text = recog.recognize_google(audio)
+        print("You said : {}".format(text))
+        file = open('SpokenOriginal.txt', 'w')
+        file.write(text)
+        file.close()
+
+        text_to_translate = translator.translate(text, src = from_lang, dest = to_lang)
+        text = text_to_translate.text
+        speak = gTTS(text=text, lang = to_lang, slow = False)
+        speak.save("TranslatedAudio.mp3")
+        os.system("start TranslatedAudio.mp3")
+
+    except:
+        print("Something Failed !")
